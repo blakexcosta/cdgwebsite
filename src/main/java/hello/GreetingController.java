ea117210e0b8c64b51854a16733285a636d54a1a
bac5899 <bac5899@g.rit.edu>
2018-08-06 17:58:57 -0400

    first homepage of CDG

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     //ensures that http get requests to /greeting are mapped to the greeting method
-    @GetMapping("/greeting")
+    @GetMapping("/greeting") //directs content to greeting
     //@RequestParam binds values of query string param name to the name parameter of the greeting method
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         //this makes the name attribute accessible to the view template by adding to the model
         model.addAttribute("name", name);
-        return "greeting";
+        return "greeting"; //this is required and returns you /greeting
     }
 }
